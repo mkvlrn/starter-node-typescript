ddebe49d3f91ff2b1428241bf87225ac56ea3e92
Mike Valeriano <mkvlrn@gmail.com>
2021-04-13 09:38:13 -0300

    Add figlet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+/* eslint-disable no-console */
+
+import figlet from "figlet";
+
+figlet("Hello World", (err, data) => {
+  if (err) {
+    console.dir(err);
+  }
+
+  console.log(data);
+});
