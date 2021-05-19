3ba37948ad1e8f9ec22bb553a021012ac8e1ccab
Mike Valeriano <mkvlrn@gmail.com>
2021-05-18 21:47:42 -0300

    Restructure, move configs to package.json, bump deps

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-node",
-  verbose: true,
-};
-
-export default config;
